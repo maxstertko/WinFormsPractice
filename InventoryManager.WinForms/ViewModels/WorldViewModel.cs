eef73b2e867cee615b922815d36a623fef673e00
Matt Class 165 True <maxstertko@yahoo.com>
2019-11-06 15:17:33 -0500

    A folder
    23:31

---
+++ InventoryManager.WinForms/ViewModels/WorldViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManager.Data;
+
+namespace InventoryManager.WinForms.ViewModels
+{
+    public class WorldViewModel
+    {
+        public string Filename { get; set; }
+        public World World { get; set; }
+    }
+}
